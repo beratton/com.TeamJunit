56ed144f3a2574d01043b88dc538fa8179853166
beratton <beratton@protonmail.com>
2023-01-02 23:17:10 +0300

    2ocak23

--- src/test/java/day11_seleniumwaits_cookies/C04_WebTables.java
+++ src/test/java/day11_seleniumwaits_cookies/C04_WebTables.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.SourceType;
 import utilities.ReusableMethods;
 import utilities.TestBase;
 
@@ -36,12 +37,27 @@ public class C04_WebTables extends TestBase {
 
         }
 //6. Web table’daki sutun sayisinin 13 olduğunu test edin
-
+List<WebElement> ucuncuSatirSutunListesi= driver.findElements(By.xpath("//tbody/tr[3]/td"));
+Assert.assertEquals(13,ucuncuSatirSutunListesi.size());
 //7. 5.sutunu yazdirin
-
+List<WebElement> besinciSutunElementleri= driver.findElements(By.xpath("//tbody/tr/td[5]"));
+        System.out.println("===============");
+        for (WebElement eachElement: besinciSutunElementleri
+             ) {
+            System.out.println(eachElement.getText());
+        }
 //8.Satir ve sutun sayisini parametre olarak alip, hucredeki bilgiyi döndüren bir method olusturun
 
+        WebElement IstenenendataElementi= getElement(3,5);
         ReusableMethods.bekle(5);
     }
+
+    private WebElement getElement(int satir, int sutun){
+        String dinamikXpath="//tbody/tr["+satir+"]/td["+sutun+"]";
+        WebElement istenenElement= driver.findElement(By.xpath(dinamikXpath));
+
+        return istenenElement;
+    }
+
 
 }
